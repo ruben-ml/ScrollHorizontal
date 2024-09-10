1ecaec819962bd566477eef24a2d1b4736489531
Rubén Muñoz López <ruben.munoz@ciberexperis.es>
2024-09-10 16:03:47 +0200

    Initial Commit

---
+++ ScrollHorizontal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollHorizontal
+//
+//  Created by Rubén Muñoz López on 10/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
